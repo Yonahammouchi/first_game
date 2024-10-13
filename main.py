78b6572627902adfd07b62d1d9c18441d5354c63
Yonahammouchi <yona.hammouchi@outlook.com>
2024-10-13 21:25:29 +0200

    add player/game in progress

--- main.py
+++ main.py
@@ -1,15 +1,37 @@
 import pygame
+from game import Game
 
 pygame.init()
 
-pygame.display.set_caption("hello")
-pygame.display.set_mode((500,300))
+
+pygame.display.set_caption("Comet Fall Game")
+screen = pygame.display.set_mode((1080,720))
+
+background = pygame.image.load("first_game/assets/bg.jpg")
+
+game = Game()
 
 running = True
 
 while running:
+
+   screen.blit(background, (0, -200))
+
+   screen.blit(game.player.image, game.player.rect)
+
+   pygame.display.flip()
+
    for event in pygame.event.get():
         if event.type == pygame.QUIT:
+
             running = False
+
             pygame.quit()
+            
             print("fermeture du jeu")
+        
+        elif event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_RIGHT:
+                game.player.move_right()
+            elif event.key ==pygame.K_LEFT:
+               game.player.move_left()
